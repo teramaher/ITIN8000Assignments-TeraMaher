6f1d9c2528d54182841d7044c69edbd11a5efcbe
teramaher <teramaher10@gmail.com>
2021-09-23 22:35:23 -0500

    adding psuedocode

--- hw2-ITIN8000-tera/main.py
+++ hw2-ITIN8000-tera/main.py
@@ -1,1 +1,59 @@
-print("test")
+#imports
+
+#assign entrees
+
+#assign sides
+
+#assign desserts
+
+#assign wines
+
+#print welcome
+
+#loop through different rolls
+
+#ask what role they want
+
+#role 1, a employee
+
+#ask for whole menu or category
+
+#print all of category by calling outall for everyone
+
+#print all of the chosen category
+
+#role 2, a customer
+
+#order choice
+#loop if there is none left of order and they want to order
+#order from menu
+
+#ask customer what they want from menu
+
+#sort the input with regex by checking if type is in there
+
+#check if entree is empty or none left and subtract if
+
+#check if side is empty or none left and subtract if
+
+#check if wine is empty or none left and subtract if
+
+#check if dessert is empty or none left and subtract if
+
+#random choice
+
+
+
+
+#roll 3 the manager
+
+#to open:
+
+#restart the menu file
+#restart the main file
+
+#to close:
+
+#prints out all of menu
+
+#sets all menu items to zero with subtract
--- hw2-ITIN8000-tera/menu.py
+++ hw2-ITIN8000-tera/menu.py
@@ -1,1 +1,63 @@
-print("test")
+#class entree
+
+#define entree numbers with randint in init function
+
+#SUBTRACT function
+#check if it is empty request
+#else if check if entered plate has one left
+#subtract one
+#return false
+#else return true
+
+#OUTALL function
+#print plate and the amount of plate left
+
+
+
+
+#class side
+
+#define side numbers with randint in init function
+
+#SUBTRACT function
+#check if it is empty request
+#else if check if entered plate has one left
+#subtract one
+#return false
+#else return true
+
+#OUTALL function
+#print plate and the amount of plate left
+
+
+
+
+#class wine
+#define wine numbers with randint in init function
+
+#SUBTRACT function
+#check if it is empty request
+#else if check if entered plate has one left
+#subtract one
+#return false
+#else return true
+
+#OUTALL function
+#print plate and the amount of plate left
+
+
+
+
+
+#class dessert
+#define dessert numbers with randint in init function
+
+#SUBTRACT function
+#check if it is empty request
+#else if check if entered plate has one left
+#subtract one
+#return false
+#else return true
+
+#OUTALL function
+#print plate and the amount of plate left
