c6e31b95b2bdd86ed5db88a59f369c5db2c0c014
teramaher <teramaher10@gmail.com>
2021-09-23 23:24:04 -0500

    added the rest of the menu functions
    exact same of the entree but with different naming

--- hw2-ITIN8000-tera/menu.py
+++ hw2-ITIN8000-tera/menu.py
@@ -42,47 +42,114 @@ class Entrees:
 
 #class side
 
-#define side numbers with randint in init function
+class Sides:
+    def __init__(self):
+        self.soupAmount = ran.randint(5, 10)
+        self.saladAmount = ran.randint(5, 10)
 
-#SUBTRACT function
-#check if it is empty request
-#else if check if entered plate has one left
-#subtract one
-#return false
-#else return true
+    def soupCount(self):
+        return self.soupAmount
 
-#OUTALL function
-#print plate and the amount of plate left
+    def saladCount(self):
+        return self.saladAmount
 
+    def subtract(self, entree):
+        if entree == "empty":
+            self.soupAmount = 0
+            self.saladAmount = 0
+        elif entree == "soup" and self.soupAmount > 0:
+            self.soupAmount = self.soupAmount - 1
+        elif entree == "salad" and self.saladAmount > 0:
+            self.saladAmount = self.saladAmount - 1
+        else:
+            return False
 
-
-
-#class wine
-#define wine numbers with randint in init function
-
-#SUBTRACT function
-#check if it is empty request
-#else if check if entered plate has one left
-#subtract one
-#return false
-#else return true
-
-#OUTALL function
-#print plate and the amount of plate left
+    def outall(self):
+        print("soup: " + str(self.soupAmount))
+        print("salad: " + str(self.saladAmount))
 
 
+class Wines:
+    def __init__(self):
+        self.merlotAmount = ran.randint(2, 5)
+        self.charAmount = ran.randint(2, 5)
+        self.noirAmount = ran.randint(2, 5)
+        self.roseAmount = ran.randint(2, 5)
+
+    def merlotCount(self):
+        return self.merlotAmount
+
+    def charCount(self):
+        return self.charAmount
+
+    def noirCount(self):
+        return self.noirAmount
+
+    def roseCount(self):
+        return self.roseAmount
+
+    def subtract(self, entree):
+        if entree == "empty":
+            self.merlotAmount = 0
+            self.charAmount = 0
+            self.noirAmount = 0
+            self.roseAmount = 0
+        if entree == "merlot" and self.merlotAmount > 0:
+            self.merlotAmount = self.merlotAmount - 1
+        elif entree == "chardonnay" and self.charAmount > 0:
+            self.charAmount = self.charAmount - 1
+        elif entree == "pinot noir" and self.noirAmount > 0:
+            self.noirAmount = self.noirAmount - 1
+        elif entree == "rose" and self.roseAmount > 0:
+            self.roseAmount = self.roseAmount - 1
+        else:
+            return False
+
+    def outall(self):
+        print("Merlot: " + str(self.merlotAmount))
+        print("Chardonnay: " + str(self.charAmount))
+        print("Pinot Noir: " + str(self.noirAmount))
+        print("Rose: " + str(self.roseAmount))
 
 
+class Desserts:
+    def __init__(self):
+        self.flanAmount = ran.randint(1, 3)
+        self.bruleeAmount = ran.randint(1, 3)
+        self.mooseAmount = ran.randint(1, 3)
+        self.cakeAmount = ran.randint(1, 3)
 
-#class dessert
-#define dessert numbers with randint in init function
+    def flanCount(self):
+        return self.flanAmount
 
-#SUBTRACT function
-#check if it is empty request
-#else if check if entered plate has one left
-#subtract one
-#return false
-#else return true
+    def bruleeCount(self):
+        return self.bruleeAmount
 
-#OUTALL function
-#print plate and the amount of plate left
+    def mooseCount(self):
+        return self.mooseAmount
+
+    def cakeCount(self):
+        return self.cakeAmount
+
+    def subtract(self, entree):
+        if entree == "empty":
+            self.flanAmount = 0
+            self.bruleeAmount = 0
+            self.mooseAmount = 0
+            self.cakeAmount = 0
+        elif entree == "flan" and self.flanAmount > 0:
+            self.flanAmount = self.flanAmount - 1
+        elif entree == "creme brulee" and self.bruleeAmount > 0:
+            self.bruleeAmount = self.bruleeAmount - 1
+        elif entree == "chocolate moose" and self.mooseAmount > 0:
+            self.mooseAmount = self.mooseAmount - 1
+        elif entree == "cheesecake" and self.cakeAmount > 0:
+            self.cakeAmount = self.cakeAmount - 1
+        else:
+            return False
+
+    def outall(self):
+        print("Flan: " + str(self.flanAmount))
+        print("Creme Brulee: " + str(self.bruleeAmount))
+        print("Moose: " + str(self.mooseAmount))
+        print("Cheesecake: " + str(self.cakeAmount))
