cb6693c7533f166f0db67f47fe0146319cd81b58
teramaher <teramaher10@gmail.com>
2021-09-23 23:12:29 -0500

    entree function

--- hw2-ITIN8000-tera/menu.py
+++ hw2-ITIN8000-tera/menu.py
@@ -1,17 +1,42 @@
+import random as ran
 #class entree
+class Entrees:
+    #define entree numbers with randint in init function
+    def __init__(self):
+        self.vegAmount = ran.randint(1, 6)
+        self.beefAmount = ran.randint(1, 6)
+        self.chickenAmount = ran.randint(1, 6)
 
-#define entree numbers with randint in init function
+    #SUBTRACT function
 
-#SUBTRACT function
-#check if it is empty request
-#else if check if entered plate has one left
-#subtract one
-#return false
-#else return true
+    def subtract(self, entree):
+        # check if it is empty request
+        if entree == "empty":
+            self.vegAmount = 0
+            self.beefAmount = 0
+            self.chickenAmount = 0
+        # else if check if entered plate has one left
+        elif entree == "chicken" and self.chickenAmount > 0:
+            # subtract one
+            self.chickenAmount = self.chickenAmount - 1
+            # return false
+            return False
+        elif entree == "beef" and self.beefAmount > 0:
+            self.beefAmount = self.beefAmount - 1
+            return False
+        elif entree == "vegetarian" and self.vegAmount > 0:
+            self.vegAmount = self.vegAmount - 1
+            return False
+        # else return true
+        else:
+            return True
 
 #OUTALL function
 #print plate and the amount of plate left
-
+    def outall(self):
+        print("chicken: " + str(self.vegAmount))
+        print("beef: " + str(self.beefAmount))
+        print("vegetarian: " + str(self.vegAmount))
 
 
 
