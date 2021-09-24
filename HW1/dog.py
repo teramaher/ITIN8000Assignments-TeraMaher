184a79e58fe88bc92073cfab3c23be9717c7f696
teramaher <teramaher10@gmail.com>
2021-09-23 20:58:08 -0500

    adding

---
+++ HW1/dog.py
@@ -0,0 +1,9 @@
+class Dog:
+    def __init__(self, name, color, breed, food):
+        self.name = name
+        self.color = color
+        self.breed = breed
+        self.food = food
+    
+dog1 = Dog("Bo", "black", "german shepard", "potatos")
+dog2 = Dog("Ranger", "brown", "german shepard", "apples")
