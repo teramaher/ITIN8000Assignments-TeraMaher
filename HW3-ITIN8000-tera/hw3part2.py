200caab1ac5f65364c7f2aedfe1ec2f1c860f514
teramaher <teramaher10@gmail.com>
2021-10-08 13:12:13 -0500

    added the files for the csv and looped through dc
    created dictionary to add to json

--- HW3-ITIN8000-tera/hw3part2.py
+++ HW3-ITIN8000-tera/hw3part2.py
@@ -1,18 +1,26 @@
 #import dif files
+import csv
+import json
 
 #function to go through the files and add them to json
-
+def make_json(dcFilePath, jsonFilePath, marvelFilePath):
     # create a dictionary
-
+    data = {}
     #open the file
-
+    with open(dcFilePath, encoding='utf-8') as csvf:
     #read through the file
-
+        csvReader = csv.DictReader(csvf)
         #loop through the rows
-
+        for rows in csvReader:
         #take out the alignment, eyes, hair, and sex for dictionary
-
-        #put the characteristics and the ownership within the character
+            characteristic = {
+                'ALIGN': rows['ALIGN'],
+                'EYE': rows['EYE'],
+                'HAIR': rows['HAIR'],
+                'SEX': rows['SEX'],
+            }
+            #put the characteristics and the ownership within the character
+            data[rows['name']] = {'Ownership': 'DC', 'Characteristic': characteristic}
 
     #do the same opening but with marvel
 
@@ -26,13 +34,17 @@
         #add the characteristic and ownership to the character
 
     #add the hiarchy of the character name
-
+    publisher = {'Character Name': data}
 
 
     #dump dict all to the json file
-
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(publisher, indent=4))
 
 
 #enter all the files
-
+dcFilePath = r'dc-wikia-data.csv'
+marvelFilePath = r'marvel-wikia-data.csv'
+jsonFilePath = r'ComicCharacters.json'
 # Call the files in the function
+make_json(dcFilePath, jsonFilePath, marvelFilePath)
