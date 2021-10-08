0e90f6a4467f7313319ac2f021d4f2c2d30f27e6
teramaher <teramaher10@gmail.com>
2021-10-08 11:19:58 -0500

    added pseudocode for the first part

--- HW3-ITIN8000-tera/hw3part1.py
+++ HW3-ITIN8000-tera/hw3part1.py
@@ -1,2 +1,37 @@
 
-print("hello")
+
+#calculate the amount of days
+
+#the day it is today
+#calculation for subtraction between dates
+#amount of days in year
+
+
+
+#get all inputs - names color and birthday
+
+
+#turn entered birthday into day element
+
+#call calculation formula
+
+#write name and color to text file
+
+#close file
+
+
+#Write the user's age in days to a binary file named Days Old
+#turn number into byte
+
+#open file and add it
+
+#close file
+
+
+#Add the user to a CSV file named UserData.csv in the order Last Name, First Name, Favorite Color, Days Old
+# creating a csv writer object
+
+# writing the fields
+#closeing file
+
+
