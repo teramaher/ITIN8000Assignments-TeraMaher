de260fff9d9f1a7503b295af51fdf1ab5ebd47d9
teramaher <teramaher10@gmail.com>
2021-10-08 11:12:16 -0500

    did pseudocode for part2

--- HW3-ITIN8000-tera/hw3part2.py
+++ HW3-ITIN8000-tera/hw3part2.py
@@ -1,2 +1,38 @@
+#import dif files
 
-print("hello")
+#function to go through the files and add them to json
+
+    # create a dictionary
+
+    #open the file
+
+    #read through the file
+
+        #loop through the rows
+
+        #take out the alignment, eyes, hair, and sex for dictionary
+
+        #put the characteristics and the ownership within the character
+
+    #do the same opening but with marvel
+
+        #translate to dict
+
+        #go through all the rows
+
+
+        #take the alignment, eyes, hair, and sex to put in characteristic dict
+
+        #add the characteristic and ownership to the character
+
+    #add the hiarchy of the character name
+
+
+
+    #dump dict all to the json file
+
+
+
+#enter all the files
+
+# Call the files in the function
