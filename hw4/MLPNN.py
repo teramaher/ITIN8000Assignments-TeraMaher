05465cb71eb0e4eb9af3a818cbd166f600b86b1e
teramaher <teramaher10@gmail.com>
2021-11-10 19:13:13 -0600

    added some more structure

--- hw4/MLPNN.py
+++ hw4/MLPNN.py
@@ -6,20 +6,18 @@ from sklearn.metrics import accuracy_score
 iris = load_iris()
 scaler = StandardScaler()
 
-train_data, test_data, train_labels, test_labels = train_test_split(iris.data,iris.target,test_size=0.2,random_state=None, shuffle=True)
 
-# we fit the train data
-scaler.fit(train_data)
-
-# scaling the train data
-train_data = scaler.transform(train_data)
-test_data = scaler.transform(test_data)
-
-mlp = MLPClassifier(hidden_layer_sizes=(10, 5), max_iter=1000)
-# let's fit the training data to our model
-mlp.fit(train_data, train_labels)
-
-predictions_train = mlp.predict(train_data)
-print(accuracy_score(predictions_train, train_labels))
-predictions_test = mlp.predict(test_data)
-print(accuracy_score(predictions_test, test_labels))
+for x in range(0, 5):
+    train_data, test_data, train_labels, test_labels = train_test_split(iris.data,iris.target,test_size=0.2,random_state=None, shuffle=True)
+    # we fit the train data
+    scaler.fit(train_data)
+    # scaling the train data
+    train_data = scaler.transform(train_data)
+    test_data = scaler.transform(test_data)
+    mlp = MLPClassifier(hidden_layer_sizes=(8, 4), max_iter=1000)
+    # let's fit the training data to our model
+    mlp.fit(train_data, train_labels)
+    predictions_train = mlp.predict(train_data)
+    #print(accuracy_score(predictions_train, train_labels))
+    predictions_test = mlp.predict(test_data)
+    print(accuracy_score(predictions_test, test_labels))
